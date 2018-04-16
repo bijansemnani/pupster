37238152ba8fa5f64d6b8a5878659b4443a5172c
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-04-16 14:33:36 -0700

    search function working

--- src/components/pages/Search/Search.js
+++ src/components/pages/Search/Search.js
@@ -1,11 +1,53 @@
 import React from "react";
-
-const Search = () => (
-  <div>
-    <h1>Search by Breed!</h1>
-    
-    
+import API from "../../../utils/API";
+<div>
+  <h1>Search by Breed</h1>
   </div>
-);
 
-export default Search;
+class SearchBar extends React.Component {
+  state = {
+    search: ""
+  }
+
+  handleInputChange = event => {
+    let value = event.target.value;
+    this.setState({
+      search: value
+    })
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.search);
+    API.search(this.state.search)
+    .then(res => {
+      console.log(res.data.message)
+      this.setState({
+        images: res.data.message
+      })
+    })
+  }
+
+  // handleChange = () => {
+  //     this.props.onUserInput(
+  //         this.refs['filterTextInput'].value,
+  //     );
+  // }
+
+  render() {
+      return (
+          <form>
+              <input
+                  type="text"
+                  placeholder="Search..."
+                  value={this.state.search}
+                  onChange={this.handleInputChange}
+              />
+              <button onClick={this.handleFormSubmit}>Search</button>
+          </form>
+          
+      );
+  }
+}
+
+export default SearchBar;
