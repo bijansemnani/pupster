e712775b76ab5e28cd504b4a4ed9e425b2894a67
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-04-16 14:20:06 -0700

    finished the logic for the discovery page

--- src/components/pages/Discover/Discover.js
+++ src/components/pages/Discover/Discover.js
@@ -1,26 +1,63 @@
 import React from "react";
 import API from "../../../utils/API";
+import "./Discover.css";
 
 class Discover extends React.Component {
   state = {
-    image : ""
+    image : "",
+    pupLove: 0,
+    message: ""
   }
 
   componentDidMount(){
-    this.searchImage();
+    this.searchImage(false);
   }
 
-  searchImage = () => {
-    API.searchImage()
-    .then(res => this.setState({image:res.data.message}))
-    .catch(err => console.log(err));
+  searchImage = (bool) => {
+      API.searchImage()
+      .then(res => {
+        if(bool){
+          this.setState({
+          image:res.data.message,
+          pupLove: this.state.pupLove + 1,
+          message: "The Pup Liked You back"
+        });
+        }
+        else {
+          this.setState({
+            image:res.data.message,
+            message: ""
+          });
+        }
+      })
+      .catch(err => console.log(err));
   }
+
+  newImage = (bool) => {
+    if (bool) {
+      let x = Math.floor(Math.random()*5);
+      if(1 === x){
+        this.searchImage(true);
+      } else {
+        this.searchImage(false);
+      }
+    }
+    else {
+      this.searchImage(false);
+    }
+  };
 
   render(){
+    console.log(this.state.pupLove);
     return (
       <div>
-        <img src={this.state.image} alt="this a dog"/>
+        <div className="tinder" style={{backgroundImage: `url(${this.state.image})`}}>
+          <button onClick={()=> this.newImage(true)}>Like</button>
+          <button onClick={()=> this.newImage(false)}>Dislike</button>
+        </div>
+        <h2>{this.state.message}</h2>
       </div>
+
     );
   }
 }
